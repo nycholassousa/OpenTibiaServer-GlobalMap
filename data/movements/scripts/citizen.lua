3869225a6f3969ff5983f8446b4587984b8975c8
Nycholas <nycholas@live.com>
2017-10-29 10:06:38 -0300

    Fix #1
    Fixed Teleport Effect

--- data/movements/scripts/citizen.lua
+++ data/movements/scripts/citizen.lua
@@ -1,12 +1,13 @@
-function onStepIn(creature, item, position, fromPosition)
-	if item.actionid > 30020 and item.actionid < 30050 then
-		local player = creature:getPlayer()
-		if player == nil then
-			return false
-		end
-
-		local town = Town(item.actionid - 30020)
-		player:setTown(town)
+function onStepIn(cid, item, position, fromPosition)
+	local player = Player(cid)
+	local position
+	if(item.actionid > 30020 and item.actionid < 30100) then
+		local townId = (item.actionid - 30020)
+		doPlayerSetTown(cid, townId)
+		doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You are the newest resident of " .. getTownName(townId) .. ".")
+		player:teleportTo(player:getTown():getTemplePosition())
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 	end
+
 	return true
 end
