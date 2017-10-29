f9ca07f06ca9294f948269ee2fc32b91e446db7a
Nycholas <nycholas@live.com>
2017-10-29 08:51:25 -0300

    Created Storages Lib
    Used for better organization

---
+++ data/lib/global/storages.lua
@@ -0,0 +1,199 @@
+-- STORAGES --
+-- Latest Storage Used: 100179
+-- Lastest Unique/Actionid Used: 2162
+
+Storage = {
+	EvilOfIsle = {
+		status = 100162,
+		start = 100000,
+		note = 100001,
+		wrongWorm = 100002,
+		mortimer = 100004,
+		corpse = 100005,
+		scuttyDone = 100006,
+		questLog = 100063
+	},
+	Rashid = {
+		rashid = 100007,
+		status = 100165,
+		willardSnakePackage = 100008,
+		briasol = 100009,
+		uzgod = 100010,
+		questLog = 100158
+	},
+	Yalahar = {
+		status = 100163,
+		palimuth = 100011,
+		palimuthNotes = 100012,
+		yalahari = 100013,
+		yalahariNotes = 100014,
+		mrWestGood = 100015,
+		mrWestBad = 100016,
+		tamerinGood = 100017,
+		tamerinBad = 100018,
+		helpPali = 100019,
+		helpAzerus = 100020,
+		fishGood = 100021,
+		fishBad = 100022,
+		matrixGood = 100023,
+		matrixBad = 100024,
+		diseasedMonsters1 = 100025,
+		diseasedMonsters2 = 100026,
+		diseasedMonsters3 = 100027,
+		quaras1 = 100028,
+		quaras2 = 100029,
+		quaras3 = 100030,
+		morik = 100031,
+		burn = 100032,
+		sewerCrowbar1 = 100033,
+		sewerCrowbar2 = 100034,
+		sewerCrowbar3 = 100035,
+		sewerCrowbar4 = 100036,
+		matrixDoor = 100037,
+		matrixChest = 100038,
+		startSoulCharm = 100039,
+		endSoulCharm = 100040,
+		tamerinMission = 100041,
+		onceSummonMorik = 100042,
+		tamerinDoor = 100043,
+		mrWestNoScare = 100044,
+		stopAskPali = 100045,
+		jumpBadWest = 100046,
+		jumpBadTamerin = 100047,
+		tortureSouls1 = 100048,
+		tortureSouls2 = 100049,
+		jumpBadFish = 100050,
+		rewardRoom = 100051,
+		outfitReward = 100052,
+		questLog = 100086
+	},
+	GreenDjinn = {
+		ubaid = 100053,
+		questStatus = 100059,
+		baaleal = 100054,
+		shauna = 100055,
+		partos = 100056,
+		alesar = 100057,
+		malor = 100058,
+		tear = 100059,
+		done = 100060,
+		nahlesarTrade100 = 100061,
+		questLog = 100062
+	},
+	BlueDjinn = {
+		questLog = 100064,
+		questStatus = 100159
+	},
+	DemonOak = {
+		demonOakParts1 = 100065,
+		demonOakParts2 = 100066,
+		demonOakParts3 = 100067,
+		demonOakParts4 = 100068,
+		demonOakEnter = 100069,
+		done = 100070,
+		checkInArea = 100071,
+		questLog = 100072,
+		reward = 100073,
+		oakThing = 100074,
+		lib = 100075,
+		confirmOldrakInfested = 100076,
+		talkingOldrak = 100077
+	},
+	Inquisition = {
+		questStatus = 100161,
+		questLog1 = 100078,
+		questLog2 = 100079,
+		questLog3 = 100080,
+		guards1 = 100081,
+		guards2 = 100082,
+		guards3 = 100083,
+		guards4 = 100084,
+		guards5 = 100085,
+		reward = 100166
+	},
+	ArenaQuest = {
+		questLog = 100087
+	},
+	BansheeQuest = {
+		questLog1 = 100088,
+		questLog2 = 100089,
+		questLog3 = 100090,
+		questLog4 = 100091,
+		questLog5 = 100092,
+		questLog6 = 100093,
+		questLog7 = 100094
+	},
+	PitsOfInferno = {
+		questLog1 = 100095,
+		questLog2 = 100096,
+		questLog3 = 100097,
+		questLog4 = 100098,
+		questLog5 = 100099,
+		questLog6 = 100100,
+		questLog7 = 100101,
+		questLog8 = 100102,
+		questLog9 = 100103,
+		questStatus = 100160
+	},
+	Annihilator = {
+		questLog = 5000
+	},
+	Behemoth = {
+		questLog = 100104
+	},
+	UnnaturalSelection = {
+		questLog = 100105,
+		questStatus = 100164
+	},
+	DreamersChallenge = {
+		questLog = 100106,
+		book = 100107
+	},
+	DemonHelmet = {
+		questLog = 100136
+	},
+	HelmetOfTheAncients = {
+		instrumentesAndDoor = 100137,
+		flames1 = 100138,
+		flames2 = 100139,
+		flames3 = 100140,
+		flames4 = 100141,
+		flames5 = 100142,
+		flames6 = 100143,
+		flames7 = 100144,
+		enterDoor = 100145,
+		switchCarrot = 100146,
+		questLog1 = 100147,
+		questLog2 = 100148,
+		questLog3 = 100149,
+		questLog4 = 100150,
+		questLog5 = 100151,
+		questLog6 = 100152,
+		questLog7 = 100153,
+		questLog8 = 100154,
+		questLog9 = 100155,
+		questLog10 = 100156,
+		questStatus = 100158
+	},
+	GrizzlyAdams = {
+		join = 100157
+	},
+	PostMan = {
+		postman1 = 100167,
+		postman2 = 100168,
+		postman3 = 100169,
+		postman4 = 100170,
+		questLog = 100171,
+		hatNPCTalk = 100172,
+		talphionTalk = 100173,
+		measurements1 = 100174,
+		measurements2 = 100175,
+		measurements3 = 100176,
+		measurements4 = 100177,
+		measurements5 = 100178,
+		measurements6 = 100179
+	}
+}
+
+GlobalStorage = {
+}
