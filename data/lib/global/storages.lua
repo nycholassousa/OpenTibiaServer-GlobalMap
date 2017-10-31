1e3cfe68cb237ac0dbd2973b727ec3a128f023c2
Nycholas <nycholas@live.com>
2017-10-31 07:05:46 -0300

    Demon Oak Added
    Added Demon Oak;
    Added missing movement: Demon helmet

--- data/lib/global/storages.lua
+++ data/lib/global/storages.lua
@@ -1,6 +1,16 @@
 -- STORAGES --
 
 Storage = {
+	DemonOak = {
+		Done = 1010,
+		Progress = 1013,
+		Squares = 1014,
+
+		AxeBlowsBird = 8288,
+		AxeBlowsLeft = 8289,
+		AxeBlowsRight = 8290,
+		AxeBlowsFace = 8291
+	},
 	AnnihilatorDone = 5000,
 	DemonHelmetDone = 100136
 }
--- data/lib/lib.lua
+++ data/lib/lib.lua
@@ -6,3 +6,4 @@ dofile('data/lib/compat/compat.lua')
 
 -- Custom Lib for global
 dofile('data/lib/global/storages.lua')  -- Storages Lib
+dofile('data/lib/global/oakPosition.lua')  -- Demon Oak Positions
