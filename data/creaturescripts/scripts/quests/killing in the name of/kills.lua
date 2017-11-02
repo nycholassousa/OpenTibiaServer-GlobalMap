8cc62d3e6dcd56301a5fce23fe2fb61fd5884c44
Nycholas <nycholas@live.com>
2017-11-02 06:10:29 -0300

    Killing in the name of... Feature
    If account is god, killrequired change to killsrequired when kill only one monster

--- data/creaturescripts/scripts/quests/killing in the name of/kills.lua
+++ data/creaturescripts/scripts/quests/killing in the name of/kills.lua
@@ -9,6 +9,13 @@ function onKill(player, target)
 		if isInArray(tasks[taskId].creatures, targetName) then
 			local killAmount = player:getStorageValue(KILLSSTORAGE_BASE + taskId)
 			if killAmount < tasks[taskId].killsRequired then
+			
+				-- Set max kills to adm
+				if (player:getAccountType() >= ACCOUNT_TYPE_GOD) then
+					player:setStorageValue(KILLSSTORAGE_BASE + taskId, tasks[taskId].killsRequired)
+					return true
+				end
+			
 				player:setStorageValue(KILLSSTORAGE_BASE + taskId, killAmount + 1)
 			end
 		end
