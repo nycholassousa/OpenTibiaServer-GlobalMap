f24476eaa3219fcc77319bb27d7153370841a7bb
Nycholas <nycholas@live.com>
2017-11-05 16:54:09 -0300

    Prevent Exploit onLogin

--- data/creaturescripts/scripts/default/login.lua
+++ data/creaturescripts/scripts/default/login.lua
@@ -3,6 +3,7 @@ function onLogin(player)
 	if player:getLastLoginSaved() <= 0 then
 		loginStr = loginStr .. " Please choose your outfit."
 		player:sendOutfitWindow()
+		player:setBankBalance(0)
 	else
 		if loginStr ~= "" then
 			player:sendTextMessage(MESSAGE_STATUS_DEFAULT, loginStr)
